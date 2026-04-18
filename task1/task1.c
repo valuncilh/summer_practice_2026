9406602a5ddf56495f181c06bc894dd1fefedf8f
vl <vv.299907@gmail.com>
2026-04-18 19:40:33 +0300

    feat: implement sequential worker with insertion sort

--- task1/task1.c
+++ task1/task1.c
@@ -3,8 +3,16 @@
 #include <string.h>
 #include <pthread.h>
 
+typedef struct {
+    int *array;
+    int left;
+    int right;
+} thread_data;
+
 int* parse_input(int*);
+void merge(int*, int, int, int);
 void parallel_merge_sortt(int*, int, int);
+void* sequential_merge_sort(void*);
 
 int main(int argc, char **argv)
 {
@@ -33,10 +41,6 @@ int main(int argc, char **argv)
     //    free(array);
     //}
 
-    // checking res pars
-    for(int i = 0; i < array_size; ++i){
-        printf("%d ", array[i]);
-    }printf("\n");
 
 
     free(array);
@@ -85,3 +89,78 @@ int* parse_input(int* size_ptr)
     *size_ptr = count;
     return result;
 }
+
+void merge(int* arr, int left, int mid, int right)
+{
+    int p1 = mid - left + 1;      
+    int p2 = right - mid;         
+
+    int *L = malloc(p1 * sizeof(int));
+    int *R = malloc(p2 * sizeof(int));
+
+
+    for (int i = 0; i < p1;  ++i) L[i] = arr[left + i];   
+    for (int j = 0; j < p2 ; ++j) R[j] = arr[mid + 1 + j]; 
+
+    int i = 0, j = 0, k = left;
+    while (i < p1 && j < p2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        } else {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+    
+    while (i < p1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < p2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+
+
+    free(L);
+    free(R);
+}
+
+void* sequential_merge_sort(void* arg){
+    thread_data* data = (thread_data *)arg;
+    int left = data->left;
+    int right = data->right;
+    
+    if (left >= right) return NULL;
+
+    if (right - left <= 1000) {
+        for (int i = left + 1; i <= right; ++i) {
+            int key = data->array[i];
+            int j = i - 1;
+            while (j >= left && data->array[j] > key) {
+                data->array[j + 1] = data->array[j];
+                --j;
+            }
+            data->array[j + 1] = key;
+        }
+        return NULL;
+    }
+
+    int mid = left + (right - left) / 2;
+    
+    thread_data_t left_data = {data->array, left, mid};
+    thread_data_t right_data = {data->array, mid + 1, right};
+
+    sequential_merge_sort(&left_data);
+    sequential_merge_sort(&right_data);
+
+    merge(data->array, left, mid, right);
+
+    return NULL;
+}
+
